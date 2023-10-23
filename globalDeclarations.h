13b7440fdee42d6540e7e505c110e49ff757ff3b
Ayala Zuriel <123731367+AyalaZuriel@users.noreply.github.com>
2023-10-23 14:50:53 +0300

    Update globalDeclarations.h

---
+++ globalDeclarations.h
@@ -0,0 +1,23 @@
+/**
+ * globalDeclatations.h
+ *  ----------------------------------------------------
+ * This  header file contains decelerations that are relevant to most functions in the project 
+*/
+
+#ifndef PROJECT_GLOBALDECLARATIONS_H
+#define PROJECT_GLOBALDECLARATIONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define INITIAL_IC 100
+#define INITIAL_DC 0
+#define MAX_SYMBOL_SIZE 31
+#define MAX_LINE_SIZE 80
+
+typedef enum {FALSE=-1,TRUE=1} bool;
+
+#endif 
+
